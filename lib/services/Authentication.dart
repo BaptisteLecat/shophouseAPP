2f1fd58b34d0636f2581b7e253860716e454b8cb
Baptiste <baptiste.lecat44@gmail.com>
2021-08-07 16:46:03 +0200

    Update Authentication.dart
    Add register, signIn and logOut function

--- lib/services/Authentication.dart
+++ lib/services/Authentication.dart
@@ -1,1 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthenticationService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return user;
+    } catch (exception) {
+      print(exception.toString());
+      return null;
+    }
+  }
+
+  Future registerInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+
+      //TODO créer un nouveau user dans firestore
+
+      return user;
+    } catch (exception) {
+      print(exception.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (exception) {
+      print(exception.toString());
+      return null;
+    }
+  }
+}
