46d105e24c29476c437e9dce0edf3275b47bcbbd
Baptiste <baptiste.lecat44@gmail.com>
2021-08-29 22:14:26 +0200

    Update input_password.dart
    Input length check

--- lib/common/widgets/inputs/input_password.dart
+++ lib/common/widgets/inputs/input_password.dart
@@ -20,9 +20,17 @@ class _InputPasswordState extends State<InputPassword> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      textInputAction: TextInputAction.next,
       controller: widget.controller,
       obscureText: widget.hidePassword,
       cursorColor: const Color(0xff3D5382),
+      validator: (password) {
+        if (password != null) {
+          if (password.length < 6) {
+            return "Minimum 6 caractères.";
+          }
+        }
+      },
       style: TextStyle(
         color: const Color(0x993D5382),
         fontSize: 18,
