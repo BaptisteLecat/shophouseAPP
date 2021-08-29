d4cc8b24db7d44df7d741cb2120d381c915736cb
Baptiste <baptiste.lecat44@gmail.com>
2021-08-29 19:38:17 +0200

    Update landing_screen.dart
    Automatically close the keyboard on the landing screen.

--- lib/screens/Auth/LandingScreen/landing_screen.dart
+++ lib/screens/Auth/LandingScreen/landing_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'components/body.dart';
+import 'package:flutter/services.dart';
 
 class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChannels.textInput.invokeMethod('TextInput.hide');
     return Scaffold(
       body: Body(),
     );
