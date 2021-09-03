3c7d26badc98962babfc70ee445c6f30a4ca1b35
Baptiste <baptiste.lecat44@gmail.com>
2021-09-03 20:04:03 +0200

    Responsive Grid Layout

--- lib/screens/home/components/products.dart
+++ lib/screens/home/components/products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shophouse/screens/home/components/productCards.dart';
 
 class Products extends StatefulWidget {
   const Products({Key? key}) : super(key: key);
@@ -11,41 +12,53 @@ class _ProductsState extends State<Products> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
         child: Column(
       children: [
-                  Flexible(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    color: Colors.green,
-                    child: Text(
-                      "Produits",
-                      style: Theme.of(context).textTheme.headline3,
-                    ),
-                    margin: EdgeInsets.only(left: 20, top: 20),
-                  )
-                ],
-              )),
-        Flexible(
-          flex: 4,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: GridView.builder(
-                itemCount: 4,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 20,
-                  childAspectRatio: 0.75,
-                ),
-                itemBuilder: (context, index) => Container(
-                      color: Colors.teal,
-                    )),
-          ),
-        ),
+        Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Text(
+                    "Produits",
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  margin: EdgeInsets.only(left: 20, top: 20),
+                )
+              ],
+            )),
+        Expanded(
+            flex: 4,
+            child: Padding(
+                padding: EdgeInsets.only(top: 30, left: 30, right: 20),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    if (constraints.maxWidth <= 380) {
+                      return GridView.builder(
+                          itemCount: 4,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 20,
+                            childAspectRatio: 0.65,
+                          ),
+                          itemBuilder: (context, index) => ProductCards());
+                    } else {
+                      return GridView.builder(
+                          itemCount: 4,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 20,
+                            childAspectRatio: 0.65,
+                          ),
+                          itemBuilder: (context, index) => ProductCards());
+                    }
+                  },
+                ))),
       ],
     ));
   }
