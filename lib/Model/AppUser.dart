cc7d2897fb1ab10e1952d3ad5dbc8fbce6f4a076
Baptiste <baptiste.lecat44@gmail.com>
2021-09-08 18:09:04 +0200

    Adding fields

--- lib/Model/AppUser.dart
+++ lib/Model/AppUser.dart
@@ -1,9 +1,16 @@
 class AppUser {
   final String uid;
+  String name;
+  String firstName;
+  String email;
 
   String get id {
     return uid;
   }
 
-  AppUser(this.uid);
+  AppUser(
+      {required this.uid,
+      required this.name,
+      required this.firstName,
+      required this.email});
 }
