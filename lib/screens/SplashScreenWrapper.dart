350c51e884df55288ca8ea9289cf5013bc29aaba
Baptiste <baptiste.lecat44@gmail.com>
2021-09-08 18:10:10 +0200

    Implementing new Auth System

--- lib/screens/SplashScreenWrapper.dart
+++ lib/screens/SplashScreenWrapper.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:shophouse/model/AppUser.dart';
-import 'package:provider/provider.dart';
+import 'package:shophouse/Model/AppUser.dart';
 import 'package:shophouse/screens/auth/LandingScreen/landing_screen.dart';
 import 'package:shophouse/screens/home/HomePage.dart';
+import 'package:shophouse/services/Auth/Authentication.dart';
 
 class SplashScreenWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    /*if (user == null) {*/
-    return LandingScreen();
-    /* } else {
+    final AppUser? user = AuthenticationService().getCurrentUser();
+    if (user == null) {
+      return LandingScreen();
+    } else {
       return HomePage();
-    }*/
+    }
   }
 }
