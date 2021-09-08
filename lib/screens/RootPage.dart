a254c0dfa03a1962baa59d02c757bd33b40c20c6
Baptiste <baptiste.lecat44@gmail.com>
2021-09-08 20:16:42 +0200

    Create RootPage.dart
    This page contain the menu and can redirect when clicking on other menu item.

---
+++ lib/screens/RootPage.dart
@@ -0,0 +1,92 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:shophouse/common/constant/colors.dart';
+import 'package:shophouse/common/widgets/menu/fab_bottom_app_bar.dart';
+import 'package:shophouse/screens/CartPage.dart';
+import 'package:shophouse/screens/home/HomePage.dart';
+import 'package:shophouse/screens/home/components/categories.dart';
+import 'package:shophouse/screens/home/components/products.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int _lastSelected = 0;
+  late Widget displayedPage;
+
+  void _selectedTab(int index) {
+    setState(() {
+      _lastSelected = index;
+      displayedPage = _getDisplayedPage();
+      print(displayedPage);
+    });
+  }
+
+  Widget _getDisplayedPage() {
+    Widget page = HomePage();
+    switch (_lastSelected) {
+      case 0:
+        page = HomePage();
+        break;
+      case 1:
+        page = CartPage();
+        break;
+      default:
+        page = HomePage();
+    }
+
+    return page;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    displayedPage = _getDisplayedPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: FABBottomAppBar(
+          backgroundColor: Colors.white,
+          color: Colors.grey,
+          selectedColor: Colors.red,
+          notchedShape: CircularNotchedRectangle(),
+          onTabSelected: _selectedTab,
+          items: [
+            FABBottomAppBarItem(
+                iconData: AssetImage("assets/icons/menu/home.png"),
+                text: 'Home'),
+            FABBottomAppBarItem(
+                iconData: AssetImage("assets/icons/menu/shopping-cart.png"),
+                text: 'Cart'),
+            FABBottomAppBarItem(
+                iconData: AssetImage("assets/icons/menu/family.png"),
+                text: 'Family'),
+            FABBottomAppBarItem(
+                iconData: AssetImage("assets/icons/menu/user.png"),
+                text: 'Account'),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+          width: 64.0,
+          height: 64.0,
+          child: new FloatingActionButton(
+            backgroundColor: primaryColor,
+            shape: new CircleBorder(),
+            elevation: 0.0,
+            child: Padding(
+                padding: EdgeInsets.all(14),
+                child: Image.asset("assets/icons/menu/search.png")),
+            onPressed: () {},
+          ),
+        ),
+        body: displayedPage);
+  }
+}
