6c579234a99cac4e1b68d8366c17e290ca3981ca
Baptiste <baptiste.lecat44@gmail.com>
2021-09-17 23:41:04 +0200

    Update colors.dart

--- lib/common/constant/colors.dart
+++ lib/common/constant/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xffFDAB53);
+const primaryColorLessOpacity = Color(0x21FDAB53);
 const secondaryColor = Color(0xff3D5382);
 const secondaryColorLessOpacity = Color(0x993D5382);
 const bodyTextColor = Color(0xffC3C3C3);
