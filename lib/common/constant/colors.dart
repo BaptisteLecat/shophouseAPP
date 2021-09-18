23de82543c9e8aec21d58b1f736c89b7bb25d2b2
Baptiste <baptiste.lecat44@gmail.com>
2021-09-18 14:49:58 +0200

    Design create cart button

--- lib/common/constant/colors.dart
+++ lib/common/constant/colors.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xffFDAB53);
+const primaryColorDarker1 = Color(0xffF69A67);
+const primaryColorDarker2 = Color(0xffE78F5B);
 const primaryColorLessOpacity = Color(0x21FDAB53);
+
 const secondaryColor = Color(0xff3D5382);
 const secondaryColorLessOpacity = Color(0x993D5382);
+
 const bodyTextColor = Color(0xffC3C3C3);
 
 const inputBackground = Color(0xffEEEEEE);
--- lib/screens/cart/cartList/CartList.dart
+++ lib/screens/cart/cartList/CartList.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:shophouse/Model/Cart.dart' as carts;
+import 'package:shophouse/common/constant/colors.dart';
 import 'package:shophouse/screens/cart/cartList/components/CartCard.dart';
 import 'package:shophouse/services/Api/repositories/user/UserFetcher.dart';
+import 'package:nice_buttons/nice_buttons.dart';
 
 class CartList extends StatefulWidget {
   const CartList({Key? key}) : super(key: key);
@@ -23,7 +27,7 @@ class _CartListState extends State<CartList> {
             textAlign: TextAlign.start,
           ),
           Text(
-            "Retrouvez les paniers que vous partagez avec vos amis.",
+            "Retrouvez vos paniers et partagez les avec vos amis.",
             style: Theme.of(context).textTheme.headline6,
           ),
         ],
@@ -41,8 +45,83 @@ class _CartListState extends State<CartList> {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              _generateHeader(),
               Expanded(
+                  flex: 2,
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _generateHeader(),
+                        SizedBox(height: 10),
+                        NiceButtons(
+                          height: 110,
+                          startColor: primaryColorDarker1,
+                          endColor: primaryColor,
+                          borderColor: primaryColorDarker2,
+                          borderRadius: 12,
+                          borderThickness: 7,
+                          stretch: true,
+                          gradientOrientation: GradientOrientation.Horizontal,
+                          onTap: (finish) {
+                            print('On tap called');
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            child: Flex(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              direction: Axis.horizontal,
+                              children: [
+                                Expanded(
+                                    flex: 2,
+                                    child: Image.asset(
+                                        "assets/images/illustrations/031-groceries.png")),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(),
+                                ),
+                                Expanded(
+                                    flex: 5,
+                                    child: Container(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Créer un panier",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headline4!
+                                                .copyWith(
+                                                    color: Colors.white,
+                                                    fontWeight:
+                                                        FontWeight.w700),
+                                          ),
+                                          Text(
+                                            "Appuyez-ici pour ajouter un nouveau panier.",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyText1!
+                                                .copyWith(
+                                                    color: Colors.white
+                                                        .withOpacity(0.7),
+                                                    fontWeight:
+                                                        FontWeight.w100),
+                                          )
+                                        ],
+                                      ),
+                                    ))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+              Expanded(
+                flex: 4,
                 child: FutureBuilder(
                     future: UserFetcher().getCarts(),
                     builder: (context, snapshot) {
@@ -54,7 +133,7 @@ class _CartListState extends State<CartList> {
                         if (snapshot.hasData && snapshot.data != null) {
                           carts.Carts listCarts = snapshot.data as carts.Carts;
                           return ListView.builder(
-                            padding: EdgeInsets.only(top: 25),
+                            padding: EdgeInsets.only(top: 0),
                             primary: false,
                             shrinkWrap: true,
                             itemCount: listCarts.cart.length,
