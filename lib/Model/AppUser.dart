d1c4dd7826d982beb509157d640113aab4a386a0
Baptiste <baptiste.lecat44@gmail.com>
2021-09-21 21:51:02 +0200

    Update AppUser.dart

--- lib/Model/AppUser.dart
+++ lib/Model/AppUser.dart
@@ -1,16 +1,46 @@
+// To parse this JSON data, do
+//
+//     final appUser = appUserFromJson(jsonString);
+
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+AppUser appUserFromJson(String str) => AppUser.fromJson(json.decode(str));
+
+String appUserToJson(AppUser data) => json.encode(data.toJson());
+
 class AppUser {
-  final String uid;
+  AppUser({
+    required this.id,
+    required this.name,
+    required this.firstName,
+    required this.email,
+    required this.token,
+  });
+
+  String id;
   String name;
   String firstName;
   String email;
+  String token;
 
-  String get id {
-    return uid;
+  String get uid {
+    return id;
   }
 
-  AppUser(
-      {required this.uid,
-      required this.name,
-      required this.firstName,
-      required this.email});
+  factory AppUser.fromJson(Map<String, dynamic> json) => AppUser(
+        id: json["id"],
+        name: json["name"],
+        firstName: json["firstName"],
+        email: json["email"],
+        token: json["token"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "firstName": firstName,
+        "email": email,
+        "token": token,
+      };
 }
