cc39e9cb3e7a4983cce038724f94103681655139
Baptiste <baptiste.lecat44@gmail.com>
2021-09-21 21:52:01 +0200

    Update UserFetcher.dart
    Implement whoAmI and register API call.

--- lib/services/Api/repositories/user/UserFetcher.dart
+++ lib/services/Api/repositories/user/UserFetcher.dart
@@ -1,10 +1,34 @@
+import 'package:shophouse/Model/AppUser.dart';
 import 'package:shophouse/Model/Cart.dart';
 import 'package:shophouse/services/Api/MainFetcher.dart';
 
 class UserFetcher extends MainFetcher {
+  UserFetcher() {
+    this.setUserToken();
+  }
+
   Future<Carts> getCarts() async {
     final response = await this.get("user/carts");
     print(response);
     return Carts.fromJson(response);
+  }
+
+  Future<AppUser> whoAmI({String? id, String? token}) async {
+    final response = await this
+        .post("login", body: (id != null) ? {"id": id} : {"token": token});
+    print(response);
+    return AppUser.fromJson(response);
+  }
+
+  Future<bool> register(
+      String id, String name, String firstName, String email) async {
+    bool registered = false;
+    final response = await this.post("register",
+        body: {"id": id, "name": name, "firstname": firstName, "email": email});
+    print(response);
+    if (response != null) {
+      registered = true;
+    }
+    return registered;
   }
 }
