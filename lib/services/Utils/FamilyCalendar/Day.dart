4c918900722b38479b8d2b83866cf875bec32f98
Baptiste Lecat <baptiste.lecat44@gmail.com>
2021-11-04 15:38:38 +0100

    Update Day.dart
    Update the class params

--- lib/services/Utils/FamilyCalendar/Day.dart
+++ lib/services/Utils/FamilyCalendar/Day.dart
@@ -1,5 +1,23 @@
+import 'package:shophouse/Model/Meal.dart';
+
 class Day {
-  int weekDayIndex;
+  /*int weekDayIndex;
   String dayNumber;
-  
+  bool isCurrentMonth;*/
+  DateTime date;
+  List<Meal> meals;
+
+  Day({required this.date, required this.meals});
+
+  int getWeekDayIndex(){
+    return this.date.weekday;
+  }
+
+  int getDayNumber(){
+    return this.date.day;
+  }
+
+  bool isCurrentMonth(){
+    return (this.date.month == DateTime.now().month);
+  }
 }
