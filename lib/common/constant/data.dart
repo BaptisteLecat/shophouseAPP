2dc04f66965c77e1b67f9968c67def7deb47cd25
Baptiste Lecat <baptiste.lecat44@gmail.com>
2021-11-05 12:04:14 +0100

    Update data.dart
    Adding list monthLabel

--- lib/common/constant/data.dart
+++ lib/common/constant/data.dart
@@ -7,3 +7,18 @@ const List<String> const_weekDayLabel = [
   "Sam",
   "Dim"
 ];
+
+const List<String> const_monthLabel = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+];
